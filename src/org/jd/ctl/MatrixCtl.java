c022fd9decc2fbca6534730f37070873bcf4476c
XiaoXing <Derrings@163.com>
2020-05-24 06:55:48 +0800

    commit

--- src/org/jd/ctl/MatrixCtl.java
+++ src/org/jd/ctl/MatrixCtl.java
@@ -1,10 +1,4 @@
 package org.jd.ctl;
 
-import javafx.scene.input.KeyCode;
-import org.jd.model.Matrix;
-import org.jd.view.MatrixPanel;
-
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 
 
